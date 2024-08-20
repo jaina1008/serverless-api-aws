f7e14079404e45be8ca5bf48a1b7daeb3e314b7c
jaina1008 <mailme.anshuljain@gmail.com>
2024-08-21 11:12:17 +1200

    reverted to flask-lambda in  app.py

--- aj-empathix/candidates/app.py
+++ aj-empathix/candidates/app.py
@@ -15,7 +15,7 @@ table = ddb.Table('candidates')
 def index():
     return json_response({"message": "Hello world - from app.py"})
 
-# Setting up candidates route to get or post candidate from/to database
+#  Entrypoint to GET and POST requests from database
 @app.route('/candidates', methods=['GET', 'POST'])
 def put_list_candidate():
     if request.method == 'GET':
@@ -24,40 +24,28 @@ def put_list_candidate():
         return json_response(candidates)
     else:
         table.put_item(Item=request.form.to_dict())
-        return json.dumps({"message":"candidate entry created"}), 200, {"Content-Type": "application/json"}
-        # return json_response({"message": "candidate entry created"})
+        return json_response({"message": "candidate entry created"})
 
 
-# @app.route('/candidates/<id>', methods=['GET', 'PATCH', 'DELETE'])
-# def get_patch_delete_candidate(id):
-#     key = {'id': id}
-#     if request.method == 'GET':
-#         candidate = table.get_item(Key=key).get('Item')
-#         if candidate:
-#             return json_response(candidate)
-#         else:
-#             return json_response({"message": "candidate not found"}, 404)
-#     elif request.method == 'PATCH':
-#         attribute_updates = {key: {'Value': value, 'Action': 'PUT'}
-#                              for key, value in request.form.items()}
-#         table.update_item(Key=key, AttributeUpdates=attribute_updates)
-#         return json_response({"message": "candidate entry updated"})
-    # else:
-    #     table.delete_item(Key=key)
-    #     return json_response({"message": "candidate entry deleted"})
+#  Entrypoint to GET and POST requests from database with specific ID
+@app.route('/candidates/<id>', methods=['GET', 'PATCH', 'DELETE'])
+def get_patch_delete_candidate(id):
+    key = {'id': id}
+    if request.method == 'GET':
+        candidate = table.get_item(Key=key).get('Item')
+        if candidate:
+            return json_response(candidate)
+        else:
+            return json_response({"message": "candidate not found"}, 404)
+    elif request.method == 'PATCH':
+        attribute_updates = {key: {'Value': value, 'Action': 'PUT'}
+                             for key, value in request.form.items()}
+        table.update_item(Key=key, AttributeUpdates=attribute_updates)
+        return json_response({"message": "candidate entry updated"})
+    else:
+        table.delete_item(Key=key)
+        return json_response({"message": "candidate entry deleted"})
 
 # Returns data and response code in JSON format
 def json_response(data, response_code=200):
-    return json.dumps(data, indent=4), response_code, {'Content-Type': 'application/json'}
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-#     import requests
-#     base_url = "https://8n6bygw2yk.execute-api.ap-southeast-2.amazonaws.com/Prod"
-#     r = requests.post(f"{base_url}/candidates", data = {
-#     "id": "1",
-#     "name": "Sam",
-#     "department": "Technology"
-#     })
-#     print(r.content)
+    return json.dumps(data), response_code, {'Content-Type': 'application/json'}
