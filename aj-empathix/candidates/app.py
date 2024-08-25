123f308477f724cf71c067121c86c18e42084016
jaina1008 <mailme.anshuljain@gmail.com>
2024-08-25 17:18:08 +1200

    debugging complete

--- aj-empathix/candidates/app.py
+++ aj-empathix/candidates/app.py
@@ -1,4 +1,3 @@
-import json
 import boto3
 from flask_lambda import FlaskLambda
 from flask import Flask, request, jsonify
@@ -29,85 +28,97 @@ def index():
 
 
 # Entrypoint to GET request from database
-@app.route('/candidates', methods=['GET'])
-def get_candidates():
+@app.route('/candidates/<id>', methods=['GET'])
+def get_candidates(id):
     try:
-        data= table.scan()['Items']
-        response= {'status': 'SUCCESS!, Here are all the candidates:',
-                   'data': data
-                   }
-        return jsonify(response)
+        key= {'id': id}
+        candidate= table.get_item(Key=key).get('Item')
+        if not candidate:
+            return json_response(f"No Entry Found for id: {id}", 404)
+        else:
+            return json_response("Candidate Found!", candidate)
     except Exception as e:
-        response= {
-            'status': 'error',
-            'message': str(e)
-        }
-        return jsonify(response)
+        return json_response("error", 501)
 
 
 # Entrypoint to POST request from database
 @app.route('/candidates', methods=['POST'])
 def create_candidate():
-
-    # try:
-    #     data = {
-    #         "id": "7",
-    #         "name": "Testing Via Hardcoded Data",
-    #         "department": "Technology"
-    #     }
-    #     table.put_item(Item=data)
-    #     response = {
-    #         "Message": "SUCCESS! Candidate entry created",
-    #         "Data Entered": data
-    #         }
-    #     return jsonify(response)
-    # except Exception as e:
-    #     response = {
-    #         "message": "Error sending via hardcoded method",
-    #         "error": str(e)
-    #     }
-    #     return jsonify(response)
-
     try:
-        # data = jsonify(request.data())
         data= request.get_json()
-        table.put_item(Item=data)
+        if not data or 'id' not in data:
+            return json_response("Missing 'id' ", 400)
 
-        response = {
-            "returned request body": data
-        }
-        return jsonify(response)
-    except Exception as e:
-        response = {
-            'message': "At line 73 and it failed",
-            'error': str(e)
-        }
-        return jsonify(response)
+        # Check if 'id' is a string
+        if not isinstance(data['id'], str):
+            return json_response("ID must be a string", 400)
+        
+        key= {'id': data['id']}
+        item= table.get_item(Key= key).get('Item')
 
+        if item:
+            return json_response("Candidate already exists", 400)
+        else:
+            table.put_item(Item= data)
+            return json_response("Candidate Created!", data, 200)
     except Exception as e:
-        response= {
-            'message': 'Raw Data is the problem',
-            'error': str(e)
-        }
-        return jsonify(response)
-    
-    try:
+        return json_response("error", 501)
+
+
+# Entrypoint to PUT request from database
+@app.route('/candidates/<id>', methods=['PUT'])
+def update_candidate(id):
+    try:    
+        data= request.get_json()
         if not data:
-            raise ValueError("No Data Provided")
-        
-        if 'id' not in data or 'name' not in data or 'department' not in data:
-            raise ValueError(f"Missing Required fields. Input Provided: {data}")
-        
-        table.put_item(Item=data)
-        response = {
-            "Message": "SUCCESS! Candidate entry created",
-            "Data Entered": data
-            }
-        return jsonify(response)
-    except Exception as e:
-        response= {
-            'message': 'Data was not added to the table',
-            'error': str(e)
-        }
-        return jsonify(response)
+            return json_response("No Data Provided", 400)
 
+        # Check if 'id' is a string
+        if not isinstance(data['id'], str):
+            return json_response("ID must be a string", 400)
+        
+        # Check if 'id' is a string
+        if not isinstance(data['id'], str):
+            return json_response("ID must be a string", 400)
+        
+        if id != data['id']:
+            return json_response("id mismatch", 400)
+        
+        item= {'id': id}
+        item.update(data)
+        table.put_item(Item= item)
+        return json_response("Candidate Updated Successfully!", item, 200)
+    except Exception as e:
+        return json_response("error", 501)
+
+
+# Entrypoint to DELETE request from database
+@app.route('/candidates/<id>', methods=['DELETE'])
+def delete_candidate(id):
+    try:
+        key= {'id': id}
+        candidate= table.get_item(Key=key).get('Item')
+
+        if not candidate:
+            return json_response(f"No Entry Found for id: {id}", 404)
+        
+        else:
+            table.delete_item(Key=key)
+            return json_response("Candidate is now Deleted")
+    except Exception as e:
+        return json_response("error", 501)
+    
+
+
+def json_response(message, *args, **kwargs):
+    response= {"Message": message}
+    if args:
+        response['args']= args
+    if kwargs:
+        response['kwargs']= kwargs
+
+    return jsonify(response)
+
+
+if __name__=="__main__":
+    app.run(debug=True)  # run the Flask app in debug mode
