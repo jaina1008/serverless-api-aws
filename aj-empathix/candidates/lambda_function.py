019dd40fd741b61bdd5a2fa5dd46e2230f9c5e46
jaina1008 <mailme.anshuljain@gmail.com>
2024-08-26 08:05:55 +1200

    comments added on lambda function

--- aj-empathix/candidates/lambda_function.py
+++ aj-empathix/candidates/lambda_function.py
@@ -1,5 +1,7 @@
 from app import app
 from serverless_wsgi import handle_request
 
+# This is the entry point for AWS Lambda function execution.
 def lambda_handler(event, context):
+    # The handle_request function processes incoming request using Flask app instance.
     return handle_request(app, event, context)
